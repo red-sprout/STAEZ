bddac3230b7051f8cd5d8a4ed9f5bed96a5f432e
red-sprout <jjw05015@gmail.com>
2024-05-22 15:32:51 +0900

    [주재완] Style : header menubar 색상 지정

---
+++ staez/src/main/webapp/resources/js/common.js
@@ -0,0 +1,13 @@
+let contextPath = "";
+function init(path) {
+    contextPath = path;
+    const url = document.location.pathname;
+    const menubarArr = [path + "/main.co", path + "/main.cm", path + "/main.no", path + "/main.iq", path + "/userList.ad"];
+    const menubar = document.querySelectorAll(".menu-bar>a");
+    for(let i in menubarArr) {
+        if(url == menubarArr[i]) {
+            menubar[i].style = "color: #B51B75";
+            break;
+        }
+    }
+}
