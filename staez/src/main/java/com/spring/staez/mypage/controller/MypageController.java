c4f368e2ccaf2082c20fd2502f619510cd871de0
prove97 <dsr102938@gmail.com>
2024-05-23 15:02:21 +0900

    [이원기] Feat : 프로필 이미지 변경 모달, 비밀번호 변경 기능(조합, 일치확인) 추가

--- staez/src/main/java/com/spring/staez/mypage/controller/MypageController.java
+++ staez/src/main/java/com/spring/staez/mypage/controller/MypageController.java
@@ -14,6 +14,7 @@ public class MypageController {
 	@Autowired
 	private MypageService mps;
 	
+	//마이페이지 메인 출력
 	@RequestMapping("main.me")
 	public String loadToMain(Model model) {
 		User m = mps.test();
@@ -23,36 +24,43 @@ public class MypageController {
 		return "mypage/mypageMain";
 	}
 	
+	//결제내역 리스트 페이지 출력
 	@RequestMapping("paymentList.me")
 	public String paymentsLog(Model model) {
 		return "mypage/paymentsLog";
 	}
 	
+	//찜목록 리스트 페이지 출력
 	@RequestMapping("scrapList.me")
 	public String myScrapList(Model model) {
 		return "mypage/myScrapList";
 	}
 
+	//한줄평 리스트 페이지 출력
 	@RequestMapping("reviewList.me")
 	public String oneLineReview(Model model) {
 		return "mypage/oneLineReview";
 	}
 	
+	//나의 작성 게시글 리스트 페이지 출력
 	@RequestMapping("boardList.me")
 	public String myBoardList(Model model) {
 		return "mypage/myBoardList";
 	}
 	
+	//나의 찜목록 게시글 리스트 페이지 출력
 	@RequestMapping("likeList.me")
 	public String likeBoardList(Model model) {
 		return "mypage/likeBoardList";
 	}
 	
+	//회원정보 변경 페이지 출력
 	@RequestMapping("updateForm.me")
 	public String updateUserForm(Model model) {
 		return "mypage/updateUserForm";
 	}
 	
+	//나의 문의내역 리스트 페이지 출력
 	@RequestMapping("inquireList.me")
 	public String myInquireList(Model model) {
 		return "mypage/myInquireList";
