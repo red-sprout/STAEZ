ee50ba3d9d0ef9173262b6bf4a6eecd91a874fa1
prove97 <dsr102938@gmail.com>
2024-05-28 21:03:39 +0900

    [이원기] Feat : 한줄평, 찜목록 더미데이터 불러오기 성공

--- staez/src/main/java/com/spring/staez/community/model/vo/Board.java
+++ staez/src/main/java/com/spring/staez/community/model/vo/Board.java
@@ -2,6 +2,8 @@ package com.spring.staez.community.model.vo;
 
 import java.sql.Timestamp;
 
+import com.spring.staez.user.model.vo.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,5 +32,7 @@ public class Board {
 	private int categoryLevel; //카테고리레벨
 	private int concertNo; //공연번호
 	private int likeCount; //좋아요 개수
+	private User user;
+	private BoardAttachment boardAttachment;
 }
 
