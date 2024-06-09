ebd35c83f9396afbe08d8dcba886c5dcf77ea962
yongjoon <missjj312@nver.com>
2024-06-10 01:13:28 +0900

    [장용준] Feat : user_mapper.xml에 user_name추가 및 회원가입 / 아이디찾기 / 비밀번호찾기 에 실명입력칸 추가                           전부 select조건에 user_name추가함

--- staez/src/main/java/com/spring/staez/user/controller/UserController.java
+++ staez/src/main/java/com/spring/staez/user/controller/UserController.java
@@ -178,8 +178,8 @@ public class UserController {
 	// 이메일로 아이디찾기
 	@ResponseBody
 	@GetMapping("findEmailCheck.me")
-	public String findEmailCheck(String checkFindEmail) {
-		String result = userService.findEmailCheck(checkFindEmail);
+	public String findEmailCheck(String checkFindEmail, String userName) {
+		String result = userService.findEmailCheck(checkFindEmail,userName);
 		return result;
 	}
 
@@ -187,52 +187,54 @@ public class UserController {
 	@PostMapping("/checkFindNewPwd.me")
 	@ResponseBody
 	public String checkFindNewPwd(@RequestParam("userId") String user_id, @RequestParam("phone") String phone,
-			@RequestParam("email") String email, HttpSession session) {
+	    @RequestParam("email") String email, @RequestParam("user_name") String userName, HttpSession session) {
+	    // 유효성 검사 및 사용자 정보 확인
+	    String user = userService.findUserByIdEmailPhone(user_id, phone, email, userName);
+	    System.out.println("user 잘 받아왔나? : " + user);
 
-		// 유효성 검사 및 사용자 정보 확인
-		String user = userService.findUserByIdEmailPhone(user_id, phone, email);
-		System.out.println("user 잘 받아왔나? : " + user);
-
-		if (user != null) {
-			// 사용자 정보를 찾았을 경우
-			// 세션으로 유저 정보 저장
-			session.setAttribute("user_id", user_id);
-			session.setAttribute("phone", phone);
-			session.setAttribute("email", email);
-			return "New Pwd Go";
-		} else {
-			// 사용자 정보를 찾지 못했을 경우
-			return "Funk New Pwd";
-		}
+	    if (user != null) {
+	        // 사용자 정보를 찾았을 경우
+	        // 세션으로 유저 정보 저장
+	        session.setAttribute("user_id", user_id);
+	        session.setAttribute("phone", phone);
+	        session.setAttribute("email", email);
+	        session.setAttribute("user_name", userName);
+	        return "New Pwd Go";
+	    } else {
+	        // 사용자 정보를 찾지 못했을 경우
+	        return "Funk New Pwd";
+	    }
 	}
 
 	// 새로운 비밀번호 저장
 	@ResponseBody
 	@PostMapping("insertNewPwd.me")
 	public String insertNewPwd(String newPassword, HttpSession session, Model model) {
-		// 디버깅 추가
-		System.out.println("새로운 비번 잘 받았음? : " + newPassword);
+	    // 디버깅 추가
+	    System.out.println("새로운 비번 잘 받았음? : " + newPassword);
 
-		// 세션에서 유저 정보를 가져온다
-		String user_id = (String) session.getAttribute("user_id");
-		String phone = (String) session.getAttribute("phone");
-		String email = (String) session.getAttribute("email");
-		System.out.println("insertNewPwd : user_id=" + user_id + ", phone=" + phone + ", email=" + email);
+	    // 세션에서 유저 정보를 가져온다
+	    String user_id = (String) session.getAttribute("user_id");
+	    String phone = (String) session.getAttribute("phone");
+	    String email = (String) session.getAttribute("email");
+	    String userName = (String) session.getAttribute("user_name");
+	    System.out.println("insertNewPwd : user_id=" + user_id + ", phone=" + phone + ", email=" + email + ", user_name=" + userName);
 
-		// 암호화 작업
-		String encPwd = bcryptPasswordEncoder.encode(newPassword);
-		System.out.println("암호화된 비밀번호 : " + encPwd);
+	    // 암호화 작업
+	    String encPwd = bcryptPasswordEncoder.encode(newPassword);
+	    System.out.println("암호화된 비밀번호 : " + encPwd);
 
-		// 새로운 비밀번호와 함께 유저 정보를 업데이트
-		int result = userService.updatePassword(user_id, phone, email, encPwd);
-		System.out.println("받아온 result : " + result);
+	    // 새로운 비밀번호와 함께 유저 정보를 업데이트
+	    int result = userService.updatePassword(user_id, phone, email, userName, encPwd);
+	    System.out.println("받아온 result : " + result);
 
-		if (result > 0) {
-			return "Password Changed";
-		} else {
-			return "no";
-		}
+	    if (result > 0) {
+	        return "Password Changed";
+	    } else {
+	        return "no";
+	    }
 	}
+
 
 	// 이메일 인증 완료하면 정보 업데이트(테이블에 이메일 데이터 있을때)
 	@ResponseBody
